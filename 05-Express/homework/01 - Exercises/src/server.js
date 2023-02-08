eeb95a64da4e668025baefe3ac184362a27da741
julianrico0938 <99848062+julianrico0938@users.noreply.github.com>
2023-02-08 18:44:25 -0500

    080223

--- 05-Express/homework/01 - Exercises/src/server.js
+++ 05-Express/homework/01 - Exercises/src/server.js
@@ -6,5 +6,26 @@ const server = express();
 
 server.use(express.json());
 
+let id = 1;
+server.post("/posts", (req, res) => {
+  const { author, title, contents } = req.body;
+
+  // if(!author || !title || !contents){
+  //     return res.status(400).json({
+  //         error: "No existe ninguna publicación con dicho título y autor indicado",
+  //     });
+  // }
+
+  if (author && title && contents) {
+     const publication = {
+        author,
+        title,
+        contents,
+        id: id++
+     };
+
+  }
+});
+
 //NO MODIFICAR EL CODIGO DE ABAJO. SE USA PARA EXPORTAR EL SERVIDOR Y CORRER LOS TESTS
 module.exports = { publications, server };
